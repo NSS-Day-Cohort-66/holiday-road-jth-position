c7ba6bca70946615d526477fc0371f234a229298
PhiferTristan <phifer.tristan@outlook.com>
2023-08-25 14:27:23 -0500

    render wip for merge

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+const mainContainer = document.querySelector("#container")
+
+const renderHTML = () => {
+    return `
+    <header class="header">
+        <h1 class="title">Holiday Road</h1>
+    </header>
+    <article class="choices">
+        <section class="choices__parks options">
+            
+        </section>
+        
+        <section class="choices__attractions options">
+            
+        </section>
+        
+        <section class="choices__eateries options">
+            
+        </section>`
+}
