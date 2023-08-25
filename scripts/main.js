3eb161841d1a9ff89de14515ae797be0aff461d3
PhiferTristan <phifer.tristan@outlook.com>
2023-08-25 14:55:25 -0500

    basic render of html in place with some minor styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
-const mainContainer = document.querySelector("#container")
+const mainContainer = document.querySelector("#container");
 
 const renderHTML = () => {
-    return `
+  mainContainer.innerHTML = `
     <header class="header">
         <h1 class="title">Holiday Road</h1>
     </header>
+
+    <aside class="savedItineraries">
+        <h2>Itineraries List</h2>
+    </aside>
+
     <article class="choices">
         <section class="choices__parks options">
             
@@ -16,6 +21,13 @@ const renderHTML = () => {
         
         <section class="choices__eateries options">
             
-        </section>`
-}
+        </section>
+    </article>
+    
+    <article class="preview">
+        <h2>Itinerary Preview</h2>
+    </article>
+    `;
+};
 
+renderHTML();
